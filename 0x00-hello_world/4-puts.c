79a0b7c48cd88889a591ee005265f6efb7c61d5a
fobiasic07 <debraobondo@gmail.com>
2022-04-07 03:27:45 -0700

    Commiting files 0 through 6 for C basic commands and actions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*main is the program that is run and returns 0 of executed properly otherwise a 1*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
