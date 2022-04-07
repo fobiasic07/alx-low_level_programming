d5d5a0d83e964de8ea88b0d9a4e4601184a0fbe8
fobiasic07 <debraobondo@gmail.com>
2022-04-07 05:15:16 -0700

    Commiting corrections in files 4,5,6 and 101

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/*main - is the program that is run and 
- * returns 0 when executed properly otherwise it may give 1 which means an error
+/*
+ * main - is the program that is run and 
+ * return:0 when executed properly otherwise it may give 1 which means an error
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main represents the main block
- * return gives 0 from perfect execution
+ * main - represents the main block
+ * return:gives 0 from perfect execution
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
